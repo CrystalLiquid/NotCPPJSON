6b4adbd9f420e935481df63573e7b484dcf704a6
CrystalLiquid <749929621@qq.com>
2024-09-20 20:24:13 +0800

    Update bson.hpp

--- src/bson.hpp
+++ src/bson.hpp
@@ -1,4 +1,5 @@
 #include "json.hpp"
+#include <cmath>
 #include <cstddef>
 #include <cstdint>
 #include <tuple>
@@ -7,15 +8,15 @@ Attention! This Lib is Only Compatible With JSON!!!
 Extra Specification Might Not Be Implemented!!!
 @*/
 enum bson_data_type {
-	str,
-	integer,
-	d64_t,
-	bool_t,
-	null,
-	object,
-	array,
-	object_void,
-	array_void
+	str=11,
+	integer=18,
+	d64_t=1,
+	bool_t=8,
+	null=10,
+	object=540,
+	array=541,
+	object_void=542,
+	array_void=543
 };
 
 enum bson_ext_full_type {
@@ -37,7 +38,7 @@ enum bson_ext_full_type {
 	min_key=-1,
 	max_key=128,
 };
-using d64 = long double;
+using d64 = double;
 using data_type = json_acc_layer_np::data_type;
 using json_map = json_acc_layer_np::json_map;
 using json = json_acc_layer_np::json;
@@ -82,7 +83,65 @@ void jsonmap_to_bsonmap(json_map& map) {
 void BSON_Serialize_BMap() {
 
 }
-
+template <typename val_type,typename bytes_type>
+int32_t calc_headsize(json& node,val_type v,bytes_type b){
+	return sizeof(v)+sizeof(b)+node.title.size();
+}
+void null_serial(std::string&final_data,json& node){
+	int8_t signed_bytes = bson_data_type::null;
+	int32_t head_size = 0;
+	head_size = sizeof(signed_bytes)+node.title.size();
+	final_data.push_back(head_size);
+	final_data.push_back(static_cast<char>(signed_bytes));
+	final_data.append(node.title);
+	
+}
+void double_serial(std::string&final_data,json& node){
+	int8_t signed_bytes = bson_data_type::d64_t;
+	int32_t head_size = 0;
+	head_size = sizeof(signed_bytes)+sizeof(double)+node.title.size();
+	final_data.push_back(head_size);
+	final_data.push_back(static_cast<char>(signed_bytes));
+	final_data.append(node.title);
+	final_data.push_back(static_cast<char>(std::get<double>(node.get_val())));
+}
+void int64_serial(std::string&final_data,json& node){
+	int8_t signed_bytes = bson_data_type::integer;
+	int32_t head_size = 0;
+	head_size = sizeof(signed_bytes)+sizeof(int64_t)+node.title.size();
+	final_data.push_back(head_size);
+	final_data.push_back(static_cast<char>(signed_bytes));
+	final_data.append(node.title);
+	final_data.push_back(static_cast<char>(std::get<int64_t>(node.get_val())));
+}
+void bool_serial(std::string&final_data,json& node){
+	int8_t signed_bytes = bson_data_type::bool_t;
+	int8_t unsigned_bytes = 10;
+	if (node.content == "true") {
+		unsigned_bytes = 1;
+	}
+	if (node.content == "false") {
+		unsigned_bytes = 0;
+	}
+	if (unsigned_bytes==10) {
+		throw node;
+	}
+	int32_t head_size = 0;
+	head_size = sizeof(signed_bytes)+sizeof(bool)+node.title.size();
+	final_data.push_back(head_size);
+	final_data.push_back(static_cast<char>(signed_bytes));
+	final_data.append(node.title);
+	final_data.push_back(static_cast<char>(unsigned_bytes));
+}
+void str_serial(std::string&final_data,json& node){
+	int8_t signed_bytes = bson_data_type::str;
+	int32_t head_size = 0;
+	head_size = sizeof(signed_bytes)+node.content.size()+node.title.size();
+	final_data.push_back(head_size);
+	final_data.push_back(static_cast<char>(signed_bytes));
+	final_data.append(node.title);
+	final_data.append(node.content);
+}
 #include <utility>
 void BSON_Serialize_JMap(json_map& map, std::string&& buf) {
 	jsonmap_to_bsonmap(map);
@@ -94,39 +153,20 @@ void BSON_Serialize_JMap(json_map& map, std::string&& buf) {
 		switch (x.type) {
 
 			case bson_data_type::null:
-				signed_bytes = 10;
-				buf.push_back(static_cast<char>(signed_bytes));
+				null_serial(buf,x);
 				break;
 			case bson_data_type::integer:
-				
+				int64_serial(buf,x);
 				break;
 			case bson_data_type::d64_t:
+				double_serial(buf,x);
 				
-				signed_bytes = 2;
-				head_size = sizeof(d64_t)+sizeof(signed_bytes)+;
-				buf.push_back(head_size);
-				
-				buf.append(x.title);
-				buf.push_back(static_cast<char>(signed_bytes));
-				
-
-				signed_bytes = 0;
-				head_size = 0;
 				break;
 			case bson_data_type::bool_t:
-				if (x.content == "true") {
-					unsigned_bytes = 1;
-				}
-				else if (x.content == "false") {
-					unsigned_bytes = 0;
-				}else {
-					std::cerr<<"Error Type\n";
-					return;
-				}
-				buf.push_back(static_cast<char>(unsigned_bytes));
+				bool_serial(buf,x);
 				break;
 			case bson_data_type::str:
-
+				str_serial(buf, x);
 				break;
 			case bson_data_type::array:
 
